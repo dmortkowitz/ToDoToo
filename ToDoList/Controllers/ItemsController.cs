abfb6e1f73373b5bae3b086e77c06b83c428341a
Guest User <Guest@epicodus-20.local>
2018-09-12 08:53:21 -0700

    add new code to better follow RESTful routing.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,30 +2,36 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 
-namespace ToDoList.Controllers 
+namespace ToDoList.Controllers
 {
-  public class ItemsController : Controller 
+  public class ItemsController : Controller
   {
 
     [HttpGet("/items")]
-    public ActionResult Index() 
+    public ActionResult Index()
     {
-      List<Item> allItems = new List<Item> {};
+      List<Item> allItems = Item.GetAll();
       return View(allItems);
     }
 
     [HttpGet("/items/new")]
-    public ActionResult CreateForm() 
+    public ActionResult CreateForm()
     {
       return View();
     }
 
+    [HttpGet("/items/{id}")]
+    public ActionResult Details(int id)
+    {
+      Item item = Item.Find(id);
+      return View(item);
+    }
+
     [HttpPost("/items")]
-    public ActionResult Create() 
+    public ActionResult Create()
     {
       Item newItem = new Item(Request.Form["new-item"]);
-      newItem.Save();
-      List<Item> allItems = Item.GetAll(); 
+      List<Item> allItems = Item.GetAll();
       return View("Index", allItems);
     }
   }
