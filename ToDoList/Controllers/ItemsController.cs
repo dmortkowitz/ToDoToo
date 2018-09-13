1866768702ff02f2367322942fb8c92ce1225260
dm <dmortkowitz@gmail.com>
2018-09-13 10:07:38 -0700

    Add Categories folder.

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,31 +2,35 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 
-namespace ToDoList.Controllers 
-{
-  public class ItemsController : Controller 
-  {
+namespace ToDoList.Controllers {
+  public class ItemsController : Controller {
 
-    [HttpGet("/items")]
-    public ActionResult Index() 
-    {
-      List<Item> allItems = new List<Item> {};
-      return View(allItems);
+    [HttpGet ("/items")]
+    public ActionResult Index () {
+      List<Item> allItems = new List<Item> { };
+      return View (allItems);
     }
 
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm() 
-    {
-      return View();
+    [HttpGet ("/items/new")]
+    public ActionResult CreateForm () {
+      return View ();
     }
 
-    [HttpPost("/items")]
-    public ActionResult Create() 
-    {
-      Item newItem = new Item(Request.Form["new-item"]);
-      newItem.Save();
-      List<Item> allItems = Item.GetAll(); 
-      return View("Index", allItems);
+    [HttpGet ("/categories/{categoryId}/items/new")]
+    public ActionResult CreateForm (int categoryId) {
+      Dictionary<string, object> model = new Dictionary<string, object> ();
+      Category category = Category.Find (categoryId);
+      return View (category);
+    }
+
+    [HttpGet ("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Details (int categoryId, int itemId) {
+      Item item = Item.Find (itemId);
+      Dictionary<string, object> model = new Dictionary<string, object> ();
+      Category category = Category.Find (categoryId);
+      model.Add ("item", item);
+      model.Add ("category", category);
+      return View (item);
     }
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,35 +1,30 @@
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 
-namespace ToDoList.Models
-{
-  public class Item
-  {
+namespace ToDoList.Models {
+  public class Item {
     private string _description;
-    private static List<Item> _instances = new List<Item> {};
-    public Item (string description)
-    {
+    private static List<Item> _instances = new List<Item> { };
+    public Item (string description) {
       _description = description;
     }
-    public string GetDescription()
-    {
+    public string GetDescription () {
       return _description;
     }
-    public void SetDescription(string newDescription)
-    {
+    public void SetDescription (string newDescription) {
       _description = newDescription;
     }
-    public static List<Item> GetAll()
-    {
+    public static List<Item> GetAll () {
       return _instances;
     }
-    public void Save()
-    {
-      _instances.Add(this); 
+    public void Save () {
+      _instances.Add (this);
     }
-    public static void ClearAll()
-    {
-      _instances.Clear();
+    public static void ClearAll () {
+      _instances.Clear ();
+    }
+    public static Item Find (int searchId) {
+      return _instances[searchId - 1];
     }
   }
 }
