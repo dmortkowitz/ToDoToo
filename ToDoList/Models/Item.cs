518484ce34f9a48ce97fd4fcbb881cb5b8e14c82
dm <dmortkowitz@gmail.com>
2018-09-17 11:54:23 -0700

    Add new code in item cs file in Models, to allow MySQL access. Untested.

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,55 +1,73 @@
-using System;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using ToDoList;
+using System;
 
-namespace ToDoList.Models {
-  public class Item {
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    private int
     private string _description;
-<<<<<<< HEAD
-    private static List<Item> _instances = new List<Item> { };
-    public Item (string description) {
-=======
-    private int _id;
-    private static List<Item> _instances = new List<Item> {};
 
-    public Item (string description)
+    public Item(string Description, int Id = 0)
     {
->>>>>>> abfb6e1f73373b5bae3b086e77c06b83c428341a
-      _description = description;
-      _instances.Add(this);
-      _id = _instances.Count;
+      _id = Id;
+      _description = Description;
     }
-    public string GetDescription () {
-      return _description;
+
+    //GETTERS AND SETTERS GO HERE
+
+    public static List<Item> GetAll()
+    {
+      List<Item> allItems = new List<Item> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM items;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int itemId = rdr.GetInt32(0);
+        string itemDescription = rdr.GetString(1);
+        Item newItem = new Item(itemDescription, itemId);
+        allItems.Add(newItem);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+          conn.Dispose();
+      }
+      return allItems;
     }
-    public void SetDescription (string newDescription) {
-      _description = newDescription;
-    }
-<<<<<<< HEAD
-    public static List<Item> GetAll () {
-      return _instances;
-    }
+
     public void Save () {
       _instances.Add (this);
     }
-    public static void ClearAll () {
-      _instances.Clear ();
-=======
+
+    public static void ClearAll ()
+    {
+    _instances.Clear ();
     public int GetId()
     {
       return _id;
     }
+
     public static List<Item> GetAll()
     {
       return _instances;
     }
+
     public void Save()
     {
       _instances.Add(this);
->>>>>>> abfb6e1f73373b5bae3b086e77c06b83c428341a
     }
-    public static Item Find (int searchId) {
+
+    public static Item Find (int searchId)
+    {
       return _instances[searchId - 1];
     }
+
     public static Item Find(int searchId)
     {
       return _instances[searchId-1];
